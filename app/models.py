ac926d3da1376e0ae1016968de6188827af3ae52
bNi <beratnzp@gmail.com>
2024-05-07 22:28:45 +0300

    fix returns

--- app/models.py
+++ app/models.py
@@ -22,14 +22,14 @@ class Model(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.title
+        return '{' + str(self.id) + '} ' + self.brand.title + ' - ' + self.title
     
 class Stock(models.Model):
     model = models.ForeignKey(Model, on_delete=models.DO_NOTHING)
     is_active = models.BooleanField(default=False)
     
     def __str__(self):
-        return '{' + str(self.id) + '} ' + self.model.brand.title + ' ' + self.model.title
+        return '{' + str(self.id) + '} ' + self.model.brand.title + ' - ' + self.model.title
     
 class Order(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
